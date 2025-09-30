408992fc0ea41ae48b4f1442402b4cfc3f7d2886
Victor Obiezue <obiezuechidera@gmail.com>
2025-09-30 18:39:05 +0100

    deploying

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-set -o errexit
+set -o errexit 
 
 pip install -r requirements.txt
 
+# Build Tailwind CSS (must happen before collectstatic)
+python manage.py tailwind build
+
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Run migrations
+python manage.py migrate --no-input
